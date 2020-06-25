6ebcf12ddb613e56683c9d53d627f8544cc85501
cptAngo <leha.andreevich@yandex.ru>
2020-06-25 22:45:02 +0300

    Add files via upload

---
+++ Semaphore/src/SemaphoreImpl.java
@@ -0,0 +1,28 @@
+
+public class SemaphoreImpl implements Semaphore {
+    private final int threadCount;
+    private int threadsInside = 0;
+
+    public SemaphoreImpl(int threadCount) {
+        this.threadCount = threadCount;
+    }
+
+    @Override
+    public synchronized void acquire() {
+        while (threadsInside >= threadCount) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        threadsInside++;
+    }
+
+    @Override
+    public synchronized void release() {
+        threadsInside--;
+        this.notify();
+    }
+}
